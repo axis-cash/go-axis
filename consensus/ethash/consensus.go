f76beaccbf1b03e667535f7367c800240f6bbb91
axisxplan <support@axis.cash>
2020-09-24 16:01:05 +0800

    rewardV6

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -484,7 +484,7 @@ func Halve(blockNumber *big.Int) *big.Int {
 func accumulateRewards(config *params.ChainConfig, statedb *state.StateDB, header *types.Header, gasReward uint64) {
 
 	var reward *big.Int
-	if header.Number.Uint64() >= 30200 {
+	if header.Number.Uint64() >= 30100 {
 		reward = accumulateRewardsV6(statedb, header)
 	}else if header.Number.Uint64() >= axisparam.XIP7() {
 		reward = accumulateRewardsV5(statedb, header)
